88505bd0046a9aa45556ff35fd1637889adb8a34
Wálisson Soares <wallyson2013@hotmail.com>
2023-12-15 22:50:19 -0300

    Add files via upload

--- pages/Graficos.py
+++ pages/Graficos.py
@@ -7,60 +7,68 @@ file_path = "penalties.csv"
 df = pd.read_csv(file_path)
 
 # Criar novas colunas para percentual de jardas em casa e fora
-df['Home_Yards_Percentage'] = df['Home_Yards'] * 100
-df['Away_Yards_Percentage'] = df['Away_Yards'] * 100
+df['Home_Yards'] = df['Home_Count'] / df['Yards']
+df['Away_Yards'] = df['Away_Count'] / df['Yards']
 
 # Criar novas colunas para percentual de contagem em casa e fora
-df['Home_Percentage'] = df['Home_Count'] / df['Count'] * 100
-df['Away_Percentage'] = df['Away_Count'] / df['Count'] * 100
+df['Home_Percentage'] = df['Home_Count'] / df['Count']
+df['Away_Percentage'] = df['Away_Count'] / df['Count']
 
 # Título do aplicativo Streamlit
 st.title("Análise de Penalties")
 
 # Adicionar filtro interativo na barra lateral
-selected_team = st.sidebar.selectbox("Selecione um time", df['Name'].unique())
+selected_name = st.sidebar.selectbox("Selecione um nome", df['Name'].unique())
 
-# Filtrar o DataFrame com base no time selecionado
-filtered_df = df[df['Name'] == selected_team]
+# Filtrar o DataFrame com base no nome selecionado
+filtered_df = df[df['Name'] == selected_name]
 
 # Mostrar os dados brutos filtrados
 st.subheader("Dados brutos filtrados")
 st.write(filtered_df)
 
-# Gráfico de barras para percentual de jardas em casa e contagem em casa
-st.subheader("Percentual de Jardas e Contagem em Casa por Time")
-bar_chart_home = alt.Chart(filtered_df).mark_bar(opacity=0.7).encode(
+# Gráfico de barras para percentual de yards e home_count por nome
+st.subheader("Percentual de Yards e Home_Count por Nome")
+bar_chart_yards_home_count = alt.Chart(filtered_df).mark_bar(opacity=0.7).encode(
     x=alt.X('Name:N', title='Time'),
-    y=alt.Y('Home_Yards_Percentage:Q', axis=alt.Axis(format='%')),
+    y=alt.Y('Yards:Q', axis=alt.Axis(format='%')),
     color=alt.Color('Name:N', scale=alt.Scale(scheme='category20')),
-    tooltip=['Name', 'Home_Yards_Percentage', 'Home_Percentage']
+    tooltip=['Name', 'Yards', 'Home_Count']
 ).properties(width=600, height=400)
 
-bar_chart_home_count = alt.Chart(filtered_df).mark_bar(opacity=0.7).encode(
+st.altair_chart(bar_chart_yards_home_count, use_container_width=True)
+
+# Gráfico de barras para Count e away_counts por nome
+st.subheader("Count e Away_Count por Nome")
+bar_chart_count_away_count = alt.Chart(filtered_df).mark_bar(opacity=0.7).encode(
     x=alt.X('Name:N', title='Time'),
-    y=alt.Y('Home_Percentage:Q', axis=alt.Axis(format='%')),
+    y=alt.Y('Count:Q'),
     color=alt.Color('Name:N', scale=alt.Scale(scheme='category20')),
-    tooltip=['Name', 'Home_Percentage', 'Home_Count']
+    tooltip=['Name', 'Count', 'Away_Count']
 ).properties(width=600, height=400)
 
-st.altair_chart(bar_chart_home, use_container_width=True)
-st.altair_chart(bar_chart_home_count, use_container_width=True)
+st.altair_chart(bar_chart_count_away_count, use_container_width=True)
 
-# Gráfico de barras para percentual de jardas fora e contagem fora
-st.subheader("Percentual de Jardas e Contagem Fora por Time")
-bar_chart_away = alt.Chart(filtered_df).mark_bar(opacity=0.7).encode(
-    x=alt.X('Name:N', title='Time'),
-    y=alt.Y('Away_Yards_Percentage:Q', axis=alt.Axis(format='%')),
-    color=alt.Color('Name:N', scale=alt.Scale(scheme='category20')),
-    tooltip=['Name', 'Away_Yards_Percentage', 'Away_Percentage']
-).properties(width=600, height=400)
+# Gráfico de dispersão para percentual de jardas por nome
+st.subheader("Percentual de jardas por nome")
+scatter_chart = alt.Chart(filtered_df).mark_circle().encode(
+    x='Name',
+    y='Home_Yards',
+    color='Name',
+    tooltip=['Name', 'Home_Yards']
+).interactive()
+st.altair_chart(scatter_chart, use_container_width=True)
 
-bar_chart_away_count = alt.Chart(filtered_df).mark_bar(opacity=0.7).encode(
-    x=alt.X('Name:N', title='Time'),
-    y=alt.Y('Away_Percentage:Q', axis=alt.Axis(format='%')),
-    color=alt.Color('Name:N', scale=alt.Scale(scheme='category20')),
-    tooltip=['Name', 'Away_Percentage', 'Away_Count']
-).properties(width=600, height=400)
+# Gráfico de pizza para percentual de contagem de penalidades em casa
+st.subheader("Percentual de penalidades em casa")
+pie_chart_home = alt.Chart(filtered_df).mark_arc().encode(
+    theta='Home_Percentage',
+    color='Home_Percentage',
+    tooltip=['Home_Percentage']
+).interactive()
+st.altair_chart(pie_chart_home, use_container_width=True)
 
-st.altair_chart(bar_chart_away, use_container_width=True)
-st.altair_chart(bar_chart_away_count, use_container_width=True)
+# Tabela com a contagem total de penalidades por nome
+st.sidebar.subheader("Contagem Total de Penalidades por Nome")
+penalties_count_per_name = df.groupby('Name')['Count'].sum().reset_index()
+st.sidebar.table(penalties_count_per_name)
