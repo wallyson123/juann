9adfc2c44e7841e669a14700a455cc3732d3637f
Wálisson Soares <wallyson2013@hotmail.com>
2023-12-15 22:43:19 -0300

    Add files via upload

--- pages/Graficos.py
+++ pages/Graficos.py
@@ -17,13 +17,19 @@ df['Away_Percentage'] = df['Away_Count'] / df['Count']
 # Título do aplicativo Streamlit
 st.title("Análise de Penalties")
 
-# Mostrar os dados brutos
-st.subheader("Dados brutos")
-st.write(df)
+# Adicionar filtro interativo na barra lateral
+selected_name = st.sidebar.selectbox("Selecione um nome", df['Name'].unique())
+
+# Filtrar o DataFrame com base no nome selecionado
+filtered_df = df[df['Name'] == selected_name]
+
+# Mostrar os dados brutos filtrados
+st.subheader("Dados brutos filtrados")
+st.write(filtered_df)
 
 # Gráfico de barras para contagem de penalidades por nome
 st.subheader("Contagem de penalidades por nome")
-bar_chart = alt.Chart(df).mark_bar().encode(
+bar_chart = alt.Chart(filtered_df).mark_bar().encode(
     x='Name',
     y='Count',
     color='Name',
@@ -33,7 +39,7 @@ st.altair_chart(bar_chart, use_container_width=True)
 
 # Gráfico de dispersão para percentual de jardas por nome
 st.subheader("Percentual de jardas por nome")
-scatter_chart = alt.Chart(df).mark_circle().encode(
+scatter_chart = alt.Chart(filtered_df).mark_circle().encode(
     x='Name',
     y='Home_Yards',
     color='Name',
@@ -43,7 +49,7 @@ st.altair_chart(scatter_chart, use_container_width=True)
 
 # Gráfico de pizza para percentual de contagem de penalidades em casa
 st.subheader("Percentual de penalidades em casa")
-pie_chart_home = alt.Chart(df).mark_arc().encode(
+pie_chart_home = alt.Chart(filtered_df).mark_arc().encode(
     theta='Home_Percentage',
     color='Home_Percentage',
     tooltip=['Home_Percentage']
